edfac63099b1d18e40e2e56704d44785254fc274
baldrick <baldrick>
2005-01-13 22:02:44 +0000

    removed multiaes.h include

--- radiochk.c
+++ radiochk.c
@@ -34,7 +34,7 @@
 /* Statusabfrage:	if( OBJEKTBAUM [BUTTON-OBJEKT].ob_state & SELECTED) */
 /*					-> 1=selektiert, 0=unselektiert						*/
 
-#include <multiaes.h>
+/*#include <multiaes.h> why both originally ? */
 #include <aes.h>
 #include <vdi.h>
 #include <stdio.h>
