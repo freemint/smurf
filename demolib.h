acd8c5e806804b1c66a3e5fe34fbd08283a76d6c
baldrick <baldrick>
2005-01-13 19:41:57 +0000

    Initial version

---
+++ demolib.h
@@ -0,0 +1,13 @@
+#ifndef _DEMOLIB_H_
+#define _DEMOLIB_H_
+
+/*  Ok.  This isn't the real file
+ * I am just building this out of the routines
+ * that exist in SMURF's source code
+ * If someone has the real one then get it into the CVS!!!
+ * baldrick
+ */
+ 
+void ice_decrunch(long *src, char *dest);
+
+#endif
